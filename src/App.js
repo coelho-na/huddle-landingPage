357f0720876d4d5caf732c1b58a3957c05227270
Nadine Coelho <nadinecoelho9@gmail.com>
2021-10-23 14:03:02 -0300

    finish card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import { Container } from "./components/styles/Container.styled.js";
+import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
-function App() {
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#ffffff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+      </>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/styles/Card.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.section`
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  margin: 40px 0;
+  padding: 60px;
+  flex-direction: ${({ layout }) => layout || "row"};
+
+  img {
+    width: 80%;
+    margin-left: 20px;
+  }
+
+  & > div {
+    flex: 1;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+    text-align: center;
+
+    img {
+      margin-left: 0px;
+    }
+  }
+`;
